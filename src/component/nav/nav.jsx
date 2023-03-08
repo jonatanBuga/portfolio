af65abfb6c4e231a2ff3251f73543d9304117475
Jonatan Bouganim <jonatanbuga@h-mhsb-nyyd-sl-Jonatan.local>
2023-03-08 16:47:02 +0200

    finishing expeience and portfolio component

--- src/component/nav/nav.jsx
+++ src/component/nav/nav.jsx
@@ -5,14 +5,16 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
+import { useState } from 'react'
 const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#')
   return (
     <nav className='navbar'>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#About"><AiOutlineUser/></a>
-      <a href="#Experiens"><BiBook/></a>
-      <a href="#Portfolio"><RiServiceLine/></a>
-      <a href="#Contact"><BiMessageSquareDetail/></a>
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#About" onClick={()=>setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#Experiens" onClick={()=>setActiveNav('#Experiens')} className={activeNav === '#Experiens' ? 'active' : ''}><BiBook/></a>
+      <a href="#Portfolio" onClick={()=>setActiveNav('#Portfolio')} className={activeNav === '#Portfolio' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#Contact" onClick={()=>setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
 }
--- src/component/portfolio/portfolio.jsx
+++ src/component/portfolio/portfolio.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 import './Portfolio.css';
+import IMG1 from '../../assets/portfolio1.png'
+import IMG3 from '../../assets/portfolio3.png'
 const Portfolio = () => {
   return (
-    <section id='Portfolio'>Portfolio</section>
+    <section id='Portfolio'>
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-img">
+            <img src={IMG1} alt="" />
+          </div>
+          <p>WorkSami is a Searching job platform for Sami Shamoon Collage's students and graduetors. 
+            The website is here to help students and graduetors of Sami Shamoon Collage to find their first job in the industry. 
+            Founders : Jonatan Buganim, Lidor Feldman, Itay Ventura and Tal Elzam(Ceo Of OfakimTech)</p>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn'>GitHub</a>
+            <a href="https://.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-img">
+            <img src={IMG3} alt="" />
+          </div>
+          <p>A portfolio page, a small summary about myself, 
+            projects I have done over time and qualities that I bring with me. 
+            For a short tour of the site, click on the live demo button</p>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn'>GitHub</a>
+            <a href="https://.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
